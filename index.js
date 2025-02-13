d2935eaeceb8103c27de07f4ca68642078be38ba
PranayChowdhury00 <pranaychowdhury00@gmail.com>
2025-02-13 11:17:34 +0600

    necessary updates

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173"], //replace with client address
+    origin: ["http://localhost:5173","https://gro-skill.web.app"], //replace with client address
     credentials: true,
   })
 );
